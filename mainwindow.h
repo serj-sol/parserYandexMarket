ef458da701a96d53086496b17a394975e7ecdd0e
slusla <s.p.solovey@yandex.ru>
2019-09-13 22:36:37 +0300

    Таблица, картинки.

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
 };
 
 #endif // MAINWINDOW_H
