c50e45ea16bbbd6fc00abf24bdbecdce449812b7
serj-sol <serjthm@gmail.com>
2019-09-13 19:34:53 +0300

    Реализован виджет для поиска товаров на яндекс маркете. Данные о товаре сохраняются в вектор.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,166 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QHBoxLayout>
+#include <QNetworkAccessManager>
+#include <QVBoxLayout>
+#include <QEventLoop>
+#include <QNetworkReply>
+#include <QDebug>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QHBoxLayout* layout = new QHBoxLayout(this);
+    QVBoxLayout* vLayout = new QVBoxLayout(this);
+    lineEdit = new QLineEdit(this);
+    textEdit = new QTextEdit(this);
+    textEdit->hide();
+    numberOfProducts = new QSpinBox(this);
+    numberOfProducts->setValue(5);
+    searchButton = new QPushButton("Search", this);
+
+    layout->addWidget(lineEdit);
+    layout->addWidget(searchButton);
+    layout->addWidget(numberOfProducts);
+
+    vLayout->addLayout(layout);
+    vLayout->addWidget(textEdit);
+    this->centralWidget()->setLayout(vLayout);
+    this->setMaximumHeight(0);
+    connect(searchButton, SIGNAL(clicked(bool)), this, SLOT(search()));
+    connect(lineEdit, SIGNAL(returnPressed()), searchButton, SLOT(click()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::search()
+{
+    lineEdit->setEnabled(false);
+    numberOfProducts->setEnabled(false);
+    searchButton->setEnabled(false);
+    if(!data.isEmpty())
+        data.clear();
+    textEdit->clear();
+    QString searchText = lineEdit->text();
+    QString searchPattern = "https://market.yandex.ru/search?&text=";
+    QString url;
+    url = searchPattern;
+    url += searchText;
+    url += '&';
+    QNetworkAccessManager manager;
+    QNetworkReply *response = manager.get(QNetworkRequest(QUrl(url)));
+    QEventLoop event;
+    connect(response,SIGNAL(finished()),&event,SLOT(quit()));
+    event.exec();
+    html.clear();
+    html = response->readAll(); // Получаем исходный код страницы
+    while(html.isEmpty())
+    {
+        qDebug() << "Не удалось получить исходный код страницы";
+        exit(1);
+    }
+
+    //textEdit->setText(html.toHtmlEscaped());
+    //textEdit->show();
+
+    for(int i = 0; i < numberOfProducts->value(); ++i)
+        createProductInfo();
+    QMessageBox::information(this, "", "Results is done");
+
+    lineEdit->setEnabled(true);
+    numberOfProducts->setEnabled(true);
+    searchButton->setEnabled(true);
+}
+QVector<QString> MainWindow::getProductData(QString data)
+{
+    QVector<QString> res; // Последовательность данных в векторе: ссылка на картинку, названиеи, цена, ссылка на товар.
+    QString pattern;    // Шаблон для поиска индекса
+
+    // Поиск картинки
+    pattern = "<img class=\"image\" src=\"";
+    int idxPicture = data.indexOf(pattern, 0);
+    idxPicture += 26;
+    QString pictureLinkProduct = "https://";
+    while (data[idxPicture] != '"')
+    {
+        pictureLinkProduct += data[idxPicture];
+        ++idxPicture;
+    }
+    qDebug() << pictureLinkProduct;
+    res.push_back(pictureLinkProduct);
+
+    // Поиск названя товара. Полное название следует сразу после ссылки на картинку
+    QString titleProduct;
+    pattern = "title=\"";
+    int idxTitle = data.indexOf(pattern, idxPicture);
+    idxTitle += 7;
+
+    while(data[idxTitle] != "\"")
+    {
+        titleProduct += data[idxTitle];
+        ++idxTitle;
+    }
+    qDebug() << titleProduct;
+    res.push_back(titleProduct);
+
+    // Поиск цены товара
+    QString priceProduct;
+    pattern = "<div class=\"price\">";
+    int idxPrice = data.indexOf(pattern , 0);
+    idxPrice += 19;
+    while(data[idxPrice] != "<")
+    {
+        priceProduct += data[idxPrice];
+        ++idxPrice;
+    }
+    qDebug() << priceProduct;
+    res.push_back(priceProduct);
+
+    // Поиск ссылки на торвар
+    QString linkProduct = "https://market.yandex.ru";
+    pattern = "href=\"";
+    int idxHref = data.indexOf(pattern, 0);
+    idxHref += 7;
+
+    while(data[idxHref] != '"')
+    {
+        linkProduct += data[idxHref];
+        ++idxHref;
+    }
+    //linkProduct += '"';
+    qDebug() << linkProduct  <<"\n***************************************************************************";
+    res.push_back(linkProduct);
+
+    return res;
+}
+void MainWindow::createProductInfo()
+{
+    int indexBegin = html.indexOf("id=\"product", index);      // Начало информации о продукте.
+    int indexEnd = html.indexOf("id=\"product", ++indexBegin); // Конец информации о продукте.
+    index = indexEnd;
+
+    QString product;
+    product.clear();
+    if(indexBegin != -1 && indexEnd != -1)
+    {
+        for(int i = indexBegin, j = 0; i < indexEnd; ++i, ++j)
+        {
+            product[j] = html[i];
+        }
+
+        data.push_back(getProductData(product));
+    }
+    else
+    {
+        qDebug() << "Подстрока не найдена";
+        exit(1);
+    }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QSpinBox>
+#include <QPushButton>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +18,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void createProductInfo();
+    QVector<QString> getProductData(QString);
+
 
 private:
     Ui::MainWindow *ui;
+    QPushButton* searchButton;
+    QLineEdit* lineEdit;
+    QString html;
+    QTextEdit* textEdit;
+    QSpinBox* numberOfProducts;
+    int index = 0;
+    QVector<QVector<QString> > data;
+private slots:
+    void search();
 };
 
 #endif // MAINWINDOW_H
