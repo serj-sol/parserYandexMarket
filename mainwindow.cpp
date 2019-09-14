63e09a438235361520f01027346824f67d51fcec
stsolovey <st.solovey@gmail.com>
2019-09-14 22:19:55 +0300

    неудачная попытка сделать ссылку

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QApplication>
-
-
+#include <QLabel>
+#include <QLineEdit>
 
 #include <QDebug>
 
@@ -11,14 +11,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Product prd;
-    prd.set_prod("name1", "link1", "price1", "img1");
-    qDebug() << prd.get_name();
+
     tableView = new QTableView(this);
-
     setCentralWidget(tableView);
-
     model = new QStandardItemModel;
+
     QStandardItem *iName;
     QStandardItem *iPrice;
     QStandardItem *iPic;
@@ -29,8 +26,6 @@ MainWindow::MainWindow(QWidget *parent) :
         horizontalHeader.append("Цена");
         horizontalHeader.append("Картинка");
     model->setHorizontalHeaderLabels(horizontalHeader);
-
-
 
     Product prd1, prd2, prd3, prd4, prd5;
     prd1.set_prod("Клавиатура Logitech Corded Keyboard K280e Black USB", "850", "https://market.yandex.ru/product--klaviatura-logitech-corded-keyboard-k280e-black-usb/10626665",  ":/imgs/imgs/1.webp");
@@ -43,27 +38,28 @@ MainWindow::MainWindow(QWidget *parent) :
     prodList << prd1 << prd2 << prd3 << prd4 << prd5;
     qDebug() << prodList.at(1).get_name();
     for (int r = 0; r < 4; r++){
-        qDebug() << "<link title=\"" + prd1.get_name() + "\" href=\"" + prd1.get_link() + "\" >";
+        qDebug() << "<a href=\"" + prodList.at(r).get_link() + "\">" + prodList.at(r).get_name() + "</a>";
 
 
+        QLabel *prodLink = new QLabel;
+        prodLink->setText("<a href=\"" + prodList.at(r).get_link() + "\">" + prodList.at(r).get_name() + "</a>");
+        prodLink->setTextFormat(Qt::RichText);
+        prodLink->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        prodLink->setOpenExternalLinks(true);
 
-
-        //iName = new QStandardItem(prodList.at(r).get_name());
-        iName = new QStandardItem("<link title=\"" + prodList.at(r).get_name() + "\" href=\"" + prodList.at(r).get_link() + "\" >");
+        iName = new QStandardItem(prodList.at(r).get_name());
         iPrice = new QStandardItem(prodList.at(r).get_price());
-
 
         QImage image(prodList.at(r).get_img());
         iPic = new QStandardItem();
         iPic->setData(QVariant(QPixmap::fromImage(image)), Qt::DecorationRole);
-
-
 
         model->setItem(r, 0, iName);
         model->setItem(r, 1, iPrice);
         model->setItem(r, 2, iPic);
 
     }
+
     tableView->setModel(model);
     tableView->resizeRowsToContents();
     tableView->resizeColumnsToContents();
@@ -73,3 +69,5 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
